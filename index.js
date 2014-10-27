fc12f421fa5121a67034521f3d32ad44d1fde459
Fabricio C Zuardi <fabricio@fabricio.org>
2014-10-27 12:25:30 -0200

    lista de jpegs para uma tag

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Config = require('./cconfig');
+var Config = require('./config');
 
 console.log(Config);
 
@@ -7,21 +7,7 @@ Instagram = require('instagram-node-lib');
 Instagram.set('client_id', Config.clientID);
 Instagram.set('client_secret', Config.clientSecret);
 
-
-//baixa a lista das últimas fotos com a tag x
-
-//gera o index.html e copia para o diretorio
-
-
-
-
-
-
-
-
-
-
-
+//create data dir
 
 
 Instagram.tags.info({
@@ -30,3 +16,48 @@ Instagram.tags.info({
     console.log(data);
   }
 });
+
+
+Instagram.tags.recent({
+  name: Config.tags[1],
+  complete: function(mediaObjects){
+    mediaObjects.forEach(function(a, b){
+        console.log(a.images.standard_resolution.url);
+    });
+    //save data to json to a file
+    // console.log(JSON.stringify(data, " ", 2));
+  }
+});
+
+
+//baixa a lista das últimas fotos com a tag x
+
+
+//gera o index.html e copia para o diretorio
+
+
+
+
+
+/*
+GET /tags/tag-name/media/recent
+
+Instagram.tags.recent({ name: 'blue' });
+Subscriptions
+
+Tag subscriptions are also available with the following methods. A callback_url is required if not specified globally, and you may also provide a verify_token if you want to keep track of which subscription is coming back. Note that while unsubscribe is identical to the generic subscriptions method below, here, unsubscribe_all only removes tag subscriptions.
+
+Instagram.tags.subscribe({ object_id: 'blue' });
+  ->  { object: 'tag',
+        object_id: 'blue',
+        aspect: 'media',
+        callback_url: 'http://your.callback/path',
+        type: 'subscription',
+        id: '#' }
+
+Instagram.tags.unsubscribe({ id: # });
+  ->  null // null is success, an error is failure
+
+Instagram.tags.unsubscribe_all();
+  ->  null // null is success, an error is failure
+*/
