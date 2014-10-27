e58e3dbbbb9c8e5327b0c9716c868f85088947fc
Fabricio C Zuardi <fabricio@fabricio.org>
2014-10-27 11:53:31 -0200

    notas e placeholders

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ Instagram = require('instagram-node-lib');
 Instagram.set('client_id', Config.clientID);
 Instagram.set('client_secret', Config.clientSecret);
 
+
+//baixa a lista das últimas fotos com a tag x
+
+//gera o index.html e copia para o diretorio
+
+
+
+
+
+
+
+
+
+
+
+
+
 Instagram.tags.info({
   name: Config.tags[1],
   complete: function(data){
---
+++ notas.md
@@ -0,0 +1,3 @@
+instalar nginx:
+
+https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-14-04-lts
