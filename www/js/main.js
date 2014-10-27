c4a9bfc75f14960fe46c08d0742fe8137b723251
Fabricio C Zuardi <fabricio@fabricio.org>
2014-10-27 15:12:30 -0200

    script em um arquivo proprio e nao no html

---
+++ www/js/main.js
@@ -0,0 +1,31 @@
+
+$(function() {
+
+    $.vegas('slideshow', {
+        backgrounds:[
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10724715_754579141280266_731910063_n.jpg' },
+            { src:'http://scontent-a.cdninstagram.com/hphotos-xfa1/t51.2885-15/10731605_384430261712105_337510511_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10724850_1491574614429095_581639191_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10729383_792647414133187_914761487_n.jpg' },
+            { src:'http://scontent-a.cdninstagram.com/hphotos-xfa1/t51.2885-15/10354330_856514497780457_1472970561_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xfa1/t51.2885-15/10727723_528828177262178_1567807751_n.jpg' },
+            { src:'http://scontent-a.cdninstagram.com/hphotos-xaf1/t51.2885-15/10735036_1574946172733788_543216949_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10731733_474191452721612_86669249_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xpa1/t51.2885-15/10518122_727701523977398_2078649757_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10724998_756876684359640_742512219_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xfa1/t51.2885-15/10724144_700821879994851_949863101_n.jpg' },
+            { src:'http://scontent-a.cdninstagram.com/hphotos-xfa1/t51.2885-15/10665488_552884194813376_1160504155_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10004292_688419831203799_1796890300_n.jpg' },
+            { src:'http://scontent-a.cdninstagram.com/hphotos-xpa1/t51.2885-15/923810_559538327511220_1993543608_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10723926_305405962984456_207730394_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10724127_577165862411674_143999840_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10724098_1548477495367303_1161784452_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xaf1/t51.2885-15/10665918_1511688615740353_974714497_n.jpg' },
+            { src:'http://scontent-b.cdninstagram.com/hphotos-xfa1/t51.2885-15/10598729_800738543324062_1365944270_n.jpg' },
+            { src:'http://scontent-a.cdninstagram.com/hphotos-xaf1/t51.2885-15/10724280_1469100100021226_431448732_n.jpg' }
+        ]
+    })('overlay', {
+        // src:'js/vegas/overlays/01.png'
+        src:'js/vegas/overlays/05.png'
+    });
+});
