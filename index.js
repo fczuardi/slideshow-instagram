2a3aa281f6aa7d8b1a2cf9c27c464cb166b74960
Fabricio C Zuardi <fabricio@fabricio.org>
2014-10-27 11:36:52 -0200

    configuracao inicial da lib p/ api do instagram

---
+++ index.js
@@ -0,0 +1,15 @@
+var Config = require('./cconfig');
+
+console.log(Config);
+
+Instagram = require('instagram-node-lib');
+
+Instagram.set('client_id', Config.clientID);
+Instagram.set('client_secret', Config.clientSecret);
+
+Instagram.tags.info({
+  name: Config.tags[1],
+  complete: function(data){
+    console.log(data);
+  }
+});
